4ac55f4ce69981f20854ca1c58ac5a373d55e3b1
Ali Walton <ali.walton@ucas-edu.net>
2025-01-29 09:52:49 -0700

    pythin practice

---
+++ practice.py
@@ -0,0 +1,29 @@
+# Ali Walton, Variables Practice Python
+
+myname= "Ali"
+
+print(myname)
+
+smallnumber = "6"
+
+largenum = "53"
+
+breakfast = "toast"
+
+favoritecolor = "purple"
+
+schoolname = "UCAS"
+
+year = "2025"
+
+print(year)
+
+eyecolor = "blue"
+
+age = "15"
+
+print(age)
+
+favoritesubject = "English"
+
+print(favoritesubject)
