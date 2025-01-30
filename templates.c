2c54a3ed66b985f5ade5be4c143100a044f6c653
Ali Walton <ali.walton@ucas-edu.net>
2025-01-30 09:52:44 -0700

    C notes

--- templates.c
+++ templates.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+char name[] = "Ali";
+int num = 42;
+float pi = 3.14;
+
 int main(void){
-    printf("Hello Worl");
+    printf("Hello %s, my age is %d and my favorite number is %f\n", name, num, pi);
     return 0;
 }
