ca66ce9bb5c1ca0541a31959b29714ee216ef72e
Ali Walton <ali.walton@ucas-edu.net>
2025-01-31 09:44:46 -0700

    First program C

--- templates.c
+++ templates.c
@@ -1,26 +1,11 @@
 #include <stdio.h>
 
-char name[] = "Ali";
-int num = 5;
-int bignum = 57;
-char breakfast[] = "Nothing";
-char favcolor[] = "purple";
-char schoolname[] = "UCAS";
-int year = 2025;
-char eyes[] = "blue";
-int age = 15;
-char favsubject[] = "English";
+char name[20];
 
 int main(void){
-    printf("%s\n", name);
-    printf("%d\n", num);
-    printf("%d\n", bignum);
-    printf("%s\n", breakfast);
-    printf("%s\n", favcolor);
-    printf("%s\n", schoolname);
-    printf("%d\n", year);
-    printf("%s\n", eyes);
-    printf("%d\n", age);
-    printf("%s\n", favsubject);
+    printf("Hello, what is your name: \n");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Hello %s", name);
      return 0;
 }
