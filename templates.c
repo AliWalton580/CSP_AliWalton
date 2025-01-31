b8e3e1e37350445d4f8c438b613dbb8acd181c02
Ali Walton <ali.walton@ucas-edu.net>
2025-01-31 09:16:13 -0700

    C Practice

--- templates.c
+++ templates.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
 char name[] = "Ali";
-int num = 42;
-float pi = 3.14;
+int num = 5;
+int bignum = 57;
+char breakfast[] = "Nothing";
+char favcolor[] = "purple";
+char schoolname[] = "UCAS";
+int year = 2025;
+char eyes[] = "blue";
+int age = 15;
+char favsubject[] = "English";
 
 int main(void){
-    printf("Hello %s, my age is %d and my favorite number is %f\n", name, num, pi);
-    return 0;
+    printf("%s\n", name);
+    printf("%d\n", num);
+    printf("%d\n", bignum);
+    printf("%s\n", breakfast);
+    printf("%s\n", favcolor);
+    printf("%s\n", schoolname);
+    printf("%d\n", year);
+    printf("%s\n", eyes);
+    printf("%d\n", age);
+    printf("%s\n", favsubject);
+     return 0;
 }
