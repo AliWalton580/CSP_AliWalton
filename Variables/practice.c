de82d6319eabbc612a2f075d155e6d88d6a2440f
AliWalton580 <ali.walton@ucas-edu.net>
2025-02-05 02:46:05 +0000

    did C practice

---
+++ Variables/practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char name[] = "Ali";
+int smallnum = 5;
+int bignum = 57;
+char breakfast[] = "nothing";
+char favcolor[] = "purple";
+char schoolname[] = "UCAS";
+int year = 2025;
+char eyes[] = "blue";
+int age = 15;
+char favoritesub[] = "english";
+
+int main(void){
+  printf("%s\n", name);
+  printf("%d\n", smallnum);
+  printf("%d\n", bignum);
+  printf("%s\n", breakfast);
+  printf("%s\n", favcolor);
+  printf("%s\n", schoolname);
+  printf("%d\n", year);
+  printf("%s\n", eyes);
+  printf("%d\n", age);
+  printf("%s\n", favoritesub);
+  return 0;  
+}
