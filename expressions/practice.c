12270e42c7b8912e67f319926f6125159e43fac0
Ali Walton <ali.walton@ucas-edu.net>
2025-02-05 09:53:08 -0700

    Worked on expression parctice C

---
+++ expressions/practice.c
@@ -0,0 +1,21 @@
+// Ali Walton, Expression Practice C
+#include <stdio.h>
+#include <math.h>
+
+int equation1 = 7-24/8*4+6;
+int equation2 = 18/3-7+2*5;
+int equation3 = 6*4/12+72/8-9;
+int equation4 = (17-6/2)+4*3;
+int equation5 = -2*(1*4-2/2)+(6+2-3);
+int equation6 = -1*((3-4*7)/5)-2*24/6;
+
+int main(void){
+    printf("%d\n", equation1);
+    printf("%d\n", equation2);
+    printf("%d\n", equation3);
+    printf("%d\n", equation4);
+    printf("%d\n", equation5);
+    printf("%d\n", equation6);
+    printf(3*(int)pow(5, 2)/15)-(5-(int)pow(2,2));
+    return 0;
+}
