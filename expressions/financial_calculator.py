5c779bcb97f556c42581f4a1c017658adc501e46
Ali Walton <ali.walton@ucas-edu.net>
2025-02-06 09:48:43 -0700

    set up for financial calculator

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -1,1 +1,39 @@
-#Ali Walton, Finacial Calculator Python
+# Ali Walton, Finacial Calculator Python
+
+# write a print statement telling the user what the program is (Budget calculator)
+
+# Ask for monthly income (User input)
+income = float(input("What is your monthly income\n"))
+# Ask for rent amount (User input)
+rent = float(input("What is your monthly rent\n"))
+# Ask for utilities amount (User input)
+utilities = float(input("What is your monthly utilities cost\n"))
+# Ask for groceries amount (User input)
+groceries = float(input("What is your monthly grocerie cost\n"))
+# Ask for transportation amount (User input)
+groceries = float(input("What is your monthly grocerie cost\n"))
+# Calculate savings as 10% of income (variable)
+
+# Calculate spending money income - (rent+utilities+groceries+transportation+savings) (variable)
+
+# Calculate percent of rent (rent/income) (variable)
+
+# Calculate percent of utilities (utilities/income) (variable)
+
+# Calculate percent of groceries (groceries/income) (variable)
+
+# Calculate percent of transportation (transportation/income) (variable)
+
+# Calculate percent of spending (spending/income) (variable)
+
+# tell user catagorie spending amount AND percent for rent ("you spend $XX>XX on rent and that is XX% of your income")
+
+# tell user catagorie spending amount AND percent for utilities ("you spend $XX>XX on utilities and that is XX% of your income")
+
+# tell user catagorie spending amount AND percent for groceries ("you spend $XX>XX on groceries and that is XX% of your income")
+
+# tell user catagorie spending amount AND percent for transportation ("you spend $XX>XX on transportaion and that is XX% of your income")
+
+# tell user catagorie spending amount AND percent for spending ("you spend $XX>XX on spending and that is XX% of your income")
+
+# tell user catagorie spending amount AND percent for savings ("you spend $XX>XX on savings and that is 10% of your income")
