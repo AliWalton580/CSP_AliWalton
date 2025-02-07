458b9f41d201f79fe96cf83e6211c2763eb3a853
Ali Walton <ali.walton@ucas-edu.net>
2025-02-07 09:53:12 -0700

    worked on financial calculator C

--- expressions/financial_calculator.c
+++ expressions/financial_calculator.c
@@ -3,15 +3,15 @@
 // write a print statement telling the user what the program is (Budget calculator)
 
 // Ask for monthly income (User input)
-
+float income;
 // Ask for rent amount (User input)
-
+float rent;
 // Ask for utilities amount (User input)
-
+float utilities;
 // Ask for groceries amount (User input)
-
+float groceries;
 // Ask for transportation amount (User input)
-
+float transportation;
 // Calculate savings as 10% of income (variable)
 
 // Calculate spending money income - (rent-utilities-groceries-transportation-savings) (variable)
@@ -39,6 +39,10 @@
 // tell user catagorie spending amount AND percent for savings ("you spend $XX.XX on savings and that is 10% of your income")
 
 int main(void){
-    
+    printf("Welcome to my program, this financial calculator will tell you your monthly spending amount on different expensses and the percentage cost of your monthly income.\n");
+    printf("%s\n", "What is your monthly income:\n");
+    scanf("%s\n", income);
+    printf("%s\n", "What is your monthly rent cost:\n");
+    scanf("%s\n", rent);
      return 0;
 }
