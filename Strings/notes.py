3d00f937fbcee2c6f67aaaa3ca3404dbcd301449
Ali Walton <ali.walton@ucas-edu.net>
2025-02-10 09:53:10 -0700

    took notes on strings python

---
+++ Strings/notes.py
@@ -0,0 +1,11 @@
+# Ali Walton, Strings Notes Python
+
+# words in programming (data type) "" ''
+
+# name = input("What is your name?(first name only please)\n").strip().capitalize()
+
+# print(f"Hello {name} welcome to my program.")
+
+name = input("Please give me a number\n").strip().capitalize()
+num = 1+name
+print(num)
