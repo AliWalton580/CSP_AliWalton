16b580fde7b19afd1932659f2aade89d0f44d7a1
Ali Walton <ali.walton@ucas-edu.net>
2025-02-11 09:53:00 -0700

    worked on name decorator python

---
+++ Strings/name_decorator.c
@@ -0,0 +1,8 @@
+//Ali Walton, Name decorator C
+#include <stdio.h>
+#include <strings.h>
+
+int main(void){
+
+     return 0;
+}
---
+++ Strings/name_decorator.py
@@ -0,0 +1,5 @@
+# Ali Walton, Name Decorator Python
+
+name = input("What is your name\n").strip().capitalize()
+
+print(f"=== {name} ===")
