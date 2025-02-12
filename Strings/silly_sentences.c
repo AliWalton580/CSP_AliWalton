83504c17ac7011cd49a32ce17936f93122682990
Ali Walton <ali.walton@ucas-edu.net>
2025-02-12 09:52:57 -0700

    worked on silly sentences

---
+++ Strings/silly_sentences.c
@@ -0,0 +1,6 @@
+//Ali Walton, Silly Sentences C
+#include <stdio.h>
+
+int main(void){
+     return 0;
+}
---
+++ Strings/silly_sentences.py
@@ -0,0 +1,7 @@
+# Ali Walton, Silly Sentences Python
+
+name = input("Give me a name:\n").strip().capitalize()
+
+verb = input("Give me an present tense verb:\n")
+
+time = ("Give me a time:\n")
