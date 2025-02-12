48b0da072d36465f336f8a8655a4c688c37e7041
AliWalton580 <ali.walton@ucas-edu.net>
2025-02-12 20:30:14 +0000

    worked on silly sentences

--- Strings/silly_sentences.py
+++ Strings/silly_sentences.py
@@ -2,6 +2,14 @@
 
 name = input("Give me a name:\n").strip().capitalize()
 
+animal = input("Give me a animal:\n")
+
 verb = input("Give me an present tense verb:\n")
 
-time = ("Give me a time:\n")
+animaltwo = input("Give me another aanimal:\n")
+
+time = input("Give me a time:\n")
+
+place = input("Give me a place:\n")
+
+print(f"{name} is {verb} from a {animaltwo} at the time {time} at {place}.")
