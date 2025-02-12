b827afc9e9454c18f675b2a86bf0d2cc997d582f
Ali Walton <ali.walton@ucas-edu.net>
2025-02-12 09:36:01 -0700

    did name decorator C

--- Strings/name_decorator.c
+++ Strings/name_decorator.c
@@ -1,8 +1,17 @@
 //Ali Walton, Name decorator C
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
+
+char name[20];
 
 int main(void){
-
+     char dec[] = "~~~ ";
+     char dec2[] = " ~~~";
+     printf("What is your name?\n");
+     scanf("%s", name);
+     strcat(dec, name);
+     printf("%s", dec);
+     strcat(name, dec2);
+     printf("%s", dec2);
      return 0;
 }
