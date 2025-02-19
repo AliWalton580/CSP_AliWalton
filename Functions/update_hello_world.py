3891ef9f211947d154c4bc2b34badb0d0e9e5fa7
Ali Walton <ali.walton@ucas-edu.net>
2025-02-19 09:53:08 -0700

    worked

---
+++ Functions/update_hello_world.py
@@ -0,0 +1,11 @@
+# Ali Walton, Update of Hello World in Pyhton
+
+name = input("Please tell me your name:\n")
+def user(name):
+    print(f"Hello {name}")
+
+    user(name)
+    user("Olivia")
+    user("Samantha")
+    user("Max")
+    user("Pedro")
