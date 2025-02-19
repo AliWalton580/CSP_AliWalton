5c4536bf9a8012c344e694df858808da24d86cf6
AliWalton580 <ali.walton@ucas-edu.net>
2025-02-19 19:47:09 +0000

    finished update hello world in python

--- Functions/update_hello_world.py
+++ Functions/update_hello_world.py
@@ -4,8 +4,8 @@ name = input("Please tell me your name:\n")
 def user(name):
     print(f"Hello {name}")
 
-    user(name)
-    user("Olivia")
-    user("Samantha")
-    user("Max")
-    user("Pedro")
+user(name)
+user("Olivia")
+user("Samantha")
+user("Max")
+user("Pepper")
