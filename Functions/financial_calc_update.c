3e1f201c8ba1c52f6ba84022513bd1684c14355f
Ali Walton <ali.walton@ucas-edu.net>
2025-02-21 09:53:08 -0700

    worked

--- Functions/financial_calc_update.c
+++ Functions/financial_calc_update.c
@@ -1,9 +1,22 @@
 //Ali Walton, Financial Calculaotr Update in C
 #include <stdio.h>
 
+float income;
+float rent;
+float utilities;
+float groceries;
+float transportation;
+float savings;
+float spending;
+
 void info(float income, float amount, char type[]){
     float per = (amount/income)*100;
     printf("you spend $%.2f on %s and that is %.0f%% of your income.\n", amount, type , per);
+}
+
+float user(char type[]){
+    char type = ("What is your monthly cost of %s?", type);
+    return 0;
 }
 
 // write a print statement telling the user what the program is (Budget calculator)
@@ -24,23 +37,14 @@ void info(float income, float amount, char type[]){
 
 int main(void){
     printf("Welcome to my program, this financial calculator will tell you your monthly spending amount on different expensses and the percentage cost of your monthly income.\n");
-    float income;
-    printf("What is your monthly income:\n");
-    scanf("%f", &income);
-    float rent;
-    printf("What is your monthly rent:\n");
-    scanf("%f", &rent);
-    float utilities;
-    printf("What is your monthly utilities cost:\n");
-    scanf("%f", &utilities);
-    float groceries;
-    printf("What is your monthly grocerie cost:\n");
-    scanf("%f", &groceries);
-    float transportation;
-    printf("What is your monthly transportation:\n");
-    scanf("%f", &transportation);
-     float savings = income*0.1;
-    float spending = income-rent-utilities-groceries-transportation-savings;
+
+     income = user("income");
+     rent = user("rent");
+     utilities = user("utilities");
+     groceries = user("groceries");
+     transportation = user("transportation");
+     savings = income*0.1;
+     spending = income-rent-utilities-groceries-transportation-savings;
 
     info(income, rent, "rent");
     info(income, utilities, "utilities");
