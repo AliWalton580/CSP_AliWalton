d2bd9905418d52a731f8988f84dc91c0c522ece6
AliWalton580 <ali.walton@ucas-edu.net>
2025-02-22 06:08:12 +0000

    finished update financial claculator in C

--- Functions/financial_calc_update.c
+++ Functions/financial_calc_update.c
@@ -1,31 +1,6 @@
 //Ali Walton, Financial Calculaotr Update in C
 #include <stdio.h>
 
-void info(float income, float amount, char type[]){
-    float per = (amount/income)*100;
-    printf("you spend $%.2f on %s and that is %.0f%% of your income.\n", amount, type , per);
-}
-
-
-// write a print statement telling the user what the program is (Budget calculator)
-
-// Ask for monthly income (User input)
-
-// Ask for rent amount (User input)
-
-// Ask for utilities amount (User input)
-
-// Ask for groceries amount (User input)
-
-// Ask for transportation amount (User input)
-
-// Calculate savings as 10% of income (variable)
-
-// Calculate spending money income - (rent-utilities-groceries-transportation-savings) (variable)
-
-int main(void){
-    printf("Welcome to my program, this financial calculator will tell you your monthly spending amount on different expensses and the percentage cost of your monthly income.\n");
-
     float income;
     float rent;
     float utilities;
@@ -34,6 +9,27 @@ int main(void){
     float spending;
     float savings;
 
+
+void info(float income, float amount, char type[]){
+    float per = (amount/income)*100;
+    printf("you spend $%.2f on %s and that is %.0f%% of your income.\n", amount, type , per);
+}
+
+float user(float amount, char type[]){
+    printf("What is you monthly cost of %s:\n", type);
+    scanf("%f", &amount);
+}
+
+int main(void){
+    printf("Welcome to my program, this financial calculator will tell you your monthly spending amount on different expensses and the percentage cost of your monthly income.\n");
+
+    float income = user(income, "income");
+    float rent = user(rent, "rent");
+    float utilities = user(utilities, "utilities");
+    float groceries = user(groceries, "groceries");
+    float transportation = user(transportation, "transportation");
+    savings = income*0.1;
+    spending = income-(rent+utilities+groceries+transportation+savings);
 
     info(income, rent, "rent");
     info(income, utilities, "utilities");
