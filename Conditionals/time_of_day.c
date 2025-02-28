ef53ea0202fca44661c4614e08ab79ad737e630f
Ali Walton <ali.walton@ucas-edu.net>
2025-02-28 09:13:38 -0700

    finished time of day

--- Conditionals/time_of_day.c
+++ Conditionals/time_of_day.c
@@ -5,18 +5,18 @@
 int main(void){
     time_t rawtime;
     struct tm * timeinfo;
-     time(&rawtime);
+    time(&rawtime);
     timeinfo = localtime(&rawtime);
     time_t now = time(NULL);
     struct tm *tm_struct = localtime(&now);
     int hour = tm_struct->tm_hour;
 
 
-    if  (hour >= 17){
-    printf("Good Evening!\n");
-    }else if (hour >=12){
+    if  (hour >= 12 && hour < 17){
     printf("Good afternoon!\n");
-    }else if (hour < 12){
+    }else if (hour >=17){
+    printf("Good evening!\n");
+    }else{
     printf("Good morning!\n");
     }
     return 0;
