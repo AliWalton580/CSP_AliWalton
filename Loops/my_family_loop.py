6d20578f6b75f51a13e3f1eccd32b46b182cfd15
AliWalton580 <ali.walton@ucas-edu.net>
2025-03-06 02:19:30 +0000

    worked

---
+++ Loops/my_family_loop.py
@@ -0,0 +1,8 @@
+# Ali Walton, My Family Loop in Pythn
+
+family = ["Charles", "Gabe", "Ali"]
+
+num = 1
+for family in family:
+    print(f"{num}. Hello {family}.")
+    num +=1
---
+++ Loops/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+# Ali Walton, Loops Notes in Python
