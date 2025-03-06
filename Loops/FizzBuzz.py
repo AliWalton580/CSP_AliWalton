521a946bc182dd6f5ad2dcec45621a4572513de8
Ali Walton <ali.walton@ucas-edu.net>
2025-03-06 09:42:10 -0700

    finished FizzBuzz in python and C

---
+++ Loops/FizzBuzz.py
@@ -0,0 +1,11 @@
+# Ali Walton, FizzBuzz in Python
+
+for num in range (1,51):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    else:
+        print(num)
