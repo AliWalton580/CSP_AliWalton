d7cbb2bef509146999caf91d33c59dc0d21133da
AliWalton580 <ali.walton@ucas-edu.net>
2025-04-29 09:36:50 -0600

    worked on final

--- unit_three_final/final.js
+++ unit_three_final/final.js
@@ -1,1 +1,13 @@
-border-radius;display
+function show(){
+    document.getElementById("extra").style.display = "block"
+    document.getElementById("paragraph").style.display = "block"
+}
+function more(){
+    if(document.getElementById("pThree").style.display === "block"){
+        document.getElementById("pThree").style.display = "none"
+        document.getElementById("btn").innerHTML ="Show More"
+    }else{
+        document.getElementById("pThree").style.display = "block"
+        document.getElementById("btn").innerHTML ="Show Less"
+    }
+}
