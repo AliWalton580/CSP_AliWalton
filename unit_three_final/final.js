cb63c9bfbfcc9a8a62bfc0a56d28a5fe65b0c8d5
AliWalton580 <ali.walton@ucas-edu.net>
2025-05-05 19:40:33 +0000

    finished website

--- unit_three_final/final.js
+++ unit_three_final/final.js
@@ -1,14 +1,14 @@
-let images =["https://images.squarespace-cdn.com/content/v1/5e7a324590664f18b1bfea0c/257b112e-7cb1-418a-9286-aa04e031dd30/snowbirdclifflodgeview.jpg" , "https://media-cdn.tripadvisor.com/media/photo-s/0b/a6/39/44/the-cliff-lodge-in-summer.jpg"
+let images =["https://images.squarespace-cdn.com/content/v1/5e7a324590664f18b1bfea0c/257b112e-7cb1-418a-9286-aa04e031dd30/snowbirdclifflodgeview.jpg" , "https://wms.org/images/Conferences/Summer/2024/Jay_Dash_Photography_2.jpg"
 ]
 
 let count = 0
 function change(){
     if(count < images.length){
-        document.getElementById("img").src = images[count]
+        document.getElementById("lodgeImage").src = images[count]
         count += 1
     }else{
         count = 0
-        document.getElementById("img").src = images[count]
+        document.getElementById("lodgeImage").src = images[count]
     }
 }
 function show(){
