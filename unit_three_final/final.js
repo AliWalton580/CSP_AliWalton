0cdbe23fc01609a553a7f226f1fa8cd25296fecc
AliWalton580 <ali.walton@ucas-edu.net>
2025-05-05 09:52:53 -0600

    worked on website

--- unit_three_final/final.js
+++ unit_three_final/final.js
@@ -1,3 +1,16 @@
+let images =["https://images.squarespace-cdn.com/content/v1/5e7a324590664f18b1bfea0c/257b112e-7cb1-418a-9286-aa04e031dd30/snowbirdclifflodgeview.jpg" , "https://media-cdn.tripadvisor.com/media/photo-s/0b/a6/39/44/the-cliff-lodge-in-summer.jpg"
+]
+
+let count = 0
+function change(){
+    if(count < images.length){
+        document.getElementById("img").src = images[count]
+        count += 1
+    }else{
+        count = 0
+        document.getElementById("img").src = images[count]
+    }
+}
 function show(){
     document.getElementById("extra").style.display = "block"
     document.getElementById("paragraph").style.display = "block"
